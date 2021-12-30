620d6b3fda2b2778a2f1dba116125e525c684531
Afonso Monteiro <joseafonsomonteiro25@gmail.com>
2021-12-30 17:08:17 +0000

    fist commit

---
+++ app/src/main/java/ipca/combustivel/cmcarlosmonteiro/MainActivity.kt
@@ -0,0 +1,11 @@
+package ipca.combustivel.cmcarlosmonteiro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
